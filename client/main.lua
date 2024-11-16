f627a795704851726c57a8186172cb0d352b8a7b
Abdelrmb <richeabdelrahim1@gmail.com>
2024-11-16 11:53:49 +0100

    changement de nom de fonction

--- client/main.lua
+++ client/main.lua
@@ -48,45 +48,45 @@ AbdelRMBUI.CreateMenu("F5", "playerInfo", "Informations du Joueur", "mainMenu")
 AbdelRMBUI.CreateMenu("F5", "playerMoney", "Portefeuille du Joueur", "mainMenu")
 AbdelRMBUI.CreateMenu("F5", "playerBills", "Factures", "mainMenu")
 
-AbdelRMBUI.AddMenuItem("F5_mainMenu", "Informations", function()
+AbdelRMBUI.Button("F5_mainMenu", "Informations", function()
     fetchPlayerInfo()
     updatePlayerInfoMenu()
     AbdelRMBUI.OpenMenu("F5", "playerInfo")
 end)
 
-AbdelRMBUI.AddMenuItem("F5_mainMenu", "Portefeuille", function()
+AbdelRMBUI.Button("F5_mainMenu", "Portefeuille", function()
     fetchPlayerInfo()
     updatePlayerMoneyMenu()
     AbdelRMBUI.OpenMenu("F5", "playerMoney")
 end)
 
-AbdelRMBUI.AddMenuItem("F5_mainMenu", "Factures", function()
+AbdelRMBUI.Button("F5_mainMenu", "Factures", function()
     fetchPlayerBills(function()
         updatePlayerBillsMenu()
         AbdelRMBUI.OpenMenu("F5", "playerBills")
     end)
 end)
 
-AbdelRMBUI.AddMenuItem("F5_mainMenu", "Quitter le menu", function()
+AbdelRMBUI.Button("F5_mainMenu", "Quitter le menu", function()
     AbdelRMBUI.CloseMenu("F5", "mainMenu")
 end)
 
 function updatePlayerInfoMenu()
     AbdelRMBUI.ClearMenu("F5_playerInfo")
-    AbdelRMBUI.AddMenuItem("F5_playerInfo", "Nom: " .. playerName)
-    AbdelRMBUI.AddMenuItem("F5_playerInfo", "ID: " .. playerId)
-    AbdelRMBUI.AddMenuItem("F5_playerInfo", "Job: " .. job)
-    AbdelRMBUI.AddMenuItem("F5_playerInfo", "Retour au Menu Principal", function()
+    AbdelRMBUI.Button("F5_playerInfo", "Nom: " .. playerName)
+    AbdelRMBUI.Button("F5_playerInfo", "ID: " .. playerId)
+    AbdelRMBUI.Button("F5_playerInfo", "Job: " .. job)
+    AbdelRMBUI.Button("F5_playerInfo", "Retour au Menu Principal", function()
         AbdelRMBUI.OpenMenu("F5", "mainMenu")
     end)
 end
 
 function updatePlayerMoneyMenu()
     AbdelRMBUI.ClearMenu("F5_playerMoney")
-    AbdelRMBUI.AddMenuItem("F5_playerMoney", "Espèces: $" .. cash)
-    AbdelRMBUI.AddMenuItem("F5_playerMoney", "Banque: $" .. bank)
-    AbdelRMBUI.AddMenuItem("F5_playerMoney", "Argent sale: $" .. dirtyMoney)
-    AbdelRMBUI.AddMenuItem("F5_playerMoney", "Retour au Menu Principal", function()
+    AbdelRMBUI.Button("F5_playerMoney", "Espèces: $" .. cash)
+    AbdelRMBUI.Button("F5_playerMoney", "Banque: $" .. bank)
+    AbdelRMBUI.Button("F5_playerMoney", "Argent sale: $" .. dirtyMoney)
+    AbdelRMBUI.Button("F5_playerMoney", "Retour au Menu Principal", function()
         AbdelRMBUI.OpenMenu("F5", "mainMenu")
     end)
 end
@@ -98,17 +98,17 @@ function updatePlayerBillsMenu()
     if #bills > 0 then
         for _, bill in ipairs(bills) do
             local label = bill.label .. " - $" .. bill.amount
-            AbdelRMBUI.AddMenuItem("F5_playerBills", label, function()
+            AbdelRMBUI.Button("F5_playerBills", label, function()
                 TriggerServerEvent('f5menu:payBill', bill.id, bill.amount, bill.target)
                 fetchPlayerBills(updatePlayerBillsMenu)
                 AbdelRMBUI.CloseMenu("F5", "playerBills")
             end)
         end
     else
-        AbdelRMBUI.AddMenuItem("F5_playerBills", "Aucune facture disponible.")
+        AbdelRMBUI.Button("F5_playerBills", "Aucune facture disponible.")
     end
 
-    AbdelRMBUI.AddMenuItem("F5_playerBills", "Retour au Menu Principal", function()
+    AbdelRMBUI.Button("F5_playerBills", "Retour au Menu Principal", function()
         AbdelRMBUI.OpenMenu("F5", "mainMenu")
     end)
 end
